e0701d300d55d5cdd394b9699543395d214a2826
popcorn <popcorn9499@gmail.com>
2020-12-08 00:41:15 -0600

    added all the events that syncthing may throw

--- Events.py
+++ Events.py
@@ -1,7 +1,7 @@
 from EventHook import EventHook
 from BaseAPI import BaseAPI
 import asyncio
-
+from Object import Object
 
 class Events(BaseAPI):
     
@@ -36,15 +36,71 @@ class Events(BaseAPI):
     async def _gatherEvents(self):
         while True:
             params = {"timeout":60, "since": self._last_seen_id}
-            data = await self.get(self.endpoint, params=params)
-            for event in data:
+            eventData = await self.get(self.endpoint, params=params)
+            for event in eventData:
                 self._last_seen_id = event["id"]
                 print(event)
-
+                eventType = event["type"]
+                data = Object(event["data"])
+                data = Object({"time": event["time"], "data": data})
+                
+                #fire any events that occured
+                if eventType == "ConfigSaved":
+                    await self.events.onConfigSaved(data)
+                elif eventType == "DeviceConnected":
+                    await self.events.onDeviceConnected(data)
+                elif eventType == "DeviceDisconnected":
+                    await self.events.onDeviceDisconnected(data)
+                elif eventType == "DeviceDiscovered":
+                    await self.events.onDeviceDiscovered(data)
+                elif eventType == "DevicePaused":
+                    await self.events.onDevicePaused(data)
+                elif eventType == "DeviceResumed":
+                    await self.events.onDeviceResumed(data)
+                elif eventType == "DeviceRejected":
+                    await self.events.onDeviceRejected(data)
+                elif eventType == "DownloadProgress":
+                    await self.events.onDownloadProgress(data)
+                elif eventType == "FolderCompletion":
+                    await self.events.onFolderCompletetion(data)
+                elif eventType == "FolderErrors":
+                    await self.events.onFolderErrors(data)
+                elif eventType == "FolderRejected":
+                    await self.events.onFolderRejected(data)
+                elif eventType == "FolderScanProgress":
+                    await self.events.onFolderScanProgress(data)
+                elif eventType == "FolderSummary":
+                    await self.events.onFolderSummary(data)
+                elif eventType == "ItemFinished":
+                    await self.events.onItemFinished(data)
+                elif eventType == "itemStarted":
+                    await self.events.onItemStarted(data)
+                elif eventType == "ListenAddressesChanged":
+                    await self.events.onListenAddressesChanged(data)
+                elif eventType == "LocalChangeDetected":
+                    await self.events.onLocalChangeDetected(data)
+                elif eventType == "LocalIndexUpdated":
+                    await self.events.onLocalIndexUpdated(data)
+                elif eventType == "LoginAttempt":
+                    await self.events.onLoginAttempt(data)
+                elif eventType == "RemoteChangeDetected":
+                    await self.events.onRemoteChangeDetected(data)
+                elif eventType == "RemoteDownloadProgress":
+                    await self.events.onRemoteDownloadProgress(data)
+                elif eventType == "RemoteIndexUpdated":
+                    await self.events.onRemoteIndexUpdated(data)
+                elif eventType == "Starting":
+                    await self.events.onStarting(data)
+                elif eventType == "StartupComplete":
+                    await self.events.onStartupComplete(data)
+                elif eventType == "StateChanged":
+                    await self.events.onStateChanged(data)
+                
 
 
 class _Events(object):
     def __init__(self):
+        self.onConfigSaved = EventHook()
         self.onDevicePaused = EventHook()
         self.onDeviceConnected = EventHook()
         self.onDeviceDisconnected = EventHook()
@@ -52,6 +108,7 @@ class _Events(object):
         self.onDevicePaused = EventHook()
         self.onDeviceRejected = EventHook()
         self.onDeviceResumed = EventHook()
+        self.onDownloadProgress = EventHook()
         self.onFolderCompletion = EventHook()
         self.onFolderErrors = EventHook()
         self.onFolderRejected = EventHook()
---
+++ Object.py
@@ -0,0 +1,3 @@
+class Object():
+    def __init__(self, attributes:dict):
+        self.__dict__ = attributes
