3b732392939bd5481741c8c49c2371a1e2fd90c6
popcorn <popcorn9499@gmail.com>
2021-01-08 21:26:06 -0600

    stopped being an idiot and wrote this properly. one file one class

--- archiveHandler.py
+++ archiveHandler.py
@@ -1,16 +1,13 @@
 import asyncio
 import subprocess
+from archiveHandlers.rarHandler import rarHandler
 
 
-
-
+SUPPORTED_LIST = {".rar": rarHandler}
 class archiveHandler:
-
-    SUPPORTED_LIST = {".rar": rarHandler}
-
     async def determineArchiveType(archive):
         returntype = None #If we cannot handle this archive please take now
-        for key,val in archiveHandler.SUPPORTED_LIST:    
+        for key,val in SUPPORTED_LIST.items():    
             if archive.endswith(key):
                 returntype = val
                 break
@@ -21,38 +18,21 @@ class archiveHandler:
     #returns true or false
     async def isSupportedArchive(file):
         result = False
-        for key,val in archiveHandler.SUPPORTED_LIST:
+        for key,val in SUPPORTED_LIST.items():
             if file.endswith(key):
                 result = True
         return result
 
     #return a list of the contents of the archive
     async def listArchive(archive):
-        handler = archiveHandler.determineArchiveType(archive)
+        handler = await archiveHandler.determineArchiveType(archive)
         return await handler.listArchive(archive)
 
     #extract the contents of the archive.
     async def extractArchive(archive,extractLocation):
-        handler = archiveHandler.determineArchiveType(archive)
+        handler = await archiveHandler.determineArchiveType(archive)
         await handler.extractArchive(archive,extractLocation)
 
 #exceptions
 
 #handlers
-class rarHandler:
-    async def listArchive(archive):
-        output = []
-        try:
-            cmdOutput = subprocess.check_output('unrar lb {0}'.format(archive), shell=True) #extract the file
-            cmdOutput = cmdOutput.decode()
-            output = cmdOutput.strip("/n")
-            output = output[:len(output)-1] #remove the '' last element in the list
-        except subprocess.CalledProcessError:
-            print("ERROR!!")
-        return output
-
-    async def extractArchive(archive,extractLocation):
-        try:
-            print(subprocess.check_output('cd {1} && unrar x -kb -y {0}'.format(archive,extractLocation), shell=True)) #extract the file
-        except subprocess.CalledProcessError:
-            print("ERROR!!")
---
+++ archiveHandlers/rarHandler.py
@@ -0,0 +1,18 @@
+import subprocess
+class rarHandler:
+    async def listArchive(archive):
+        output = []
+        try:
+            cmdOutput = subprocess.check_output('unrar lb {0}'.format(archive), shell=True) #extract the file
+            cmdOutput = cmdOutput.decode()
+            output = cmdOutput.strip("/n")
+            output = output[:len(output)-1] #remove the '' last element in the list
+        except subprocess.CalledProcessError:
+            print("ERROR!!")
+        return output
+
+    async def extractArchive(archive,extractLocation):
+        try:
+            print(subprocess.check_output('cd {1} && unrar x -kb -y {0}'.format(archive,extractLocation), shell=True)) #extract the file
+        except subprocess.CalledProcessError:
+            print("ERROR!!")
