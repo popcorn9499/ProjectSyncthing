6ed23694d18b780b209a84f0400a128dd696cee7
popcorn <popcorn9499@gmail.com>
2021-01-10 00:09:20 -0600

    used a more direct path to unrar

--- archiveHandlers/rarHandler.py
+++ archiveHandlers/rarHandler.py
@@ -6,7 +6,7 @@ class rarHandler:
     async def listArchive(archive):
         output = []
         try:
-            cmdOutput = subprocess.check_output('unrar lb {0}'.format(archive), shell=True) #extract the file
+            cmdOutput = subprocess.check_output('/usr/bin/unrar lb {0}'.format(archive), shell=True) #extract the file
             cmdOutput = cmdOutput.decode()
             output = cmdOutput.split("\n")
             output = output[:len(output)-1] #remove the '' last element in the list
@@ -17,6 +17,6 @@ class rarHandler:
     #change directory to the extraction location and unrar
     async def extractArchive(archive,extractLocation):
         try:
-            print(subprocess.check_output('cd {1} && unrar x -kb -y {0}'.format(archive,extractLocation), shell=True)) #extract the file
+            print(subprocess.check_output('cd {1} && /usr/bin/unrar x -kb -y {0}'.format(archive,extractLocation), shell=True)) #extract the file
         except subprocess.CalledProcessError:
             print("ERROR!!")
