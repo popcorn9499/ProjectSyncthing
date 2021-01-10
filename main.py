13dc39de17553adb99c9a25d5d1d8800d296fa7d
popcorn <popcorn9499@gmail.com>
2021-01-09 20:01:35 -0600

    prevent us from processing the queue when still starting/cleaning

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ class main:
         self.events.Events.onItemFinished += self.addItemToQueue
         self.queue = [] #store a queue of file changes. This should be type list<QueueItem>
         loop = asyncio.get_event_loop()
-        self.processingQueueTask = loop.create_task(self._processQueue("NOT MEEE")) #holds the last processing queue task.
-        loop.create_task(self.startingCleaning())
+        self.processingQueueTask = loop.create_task(self._startingCleaning()) #holds the last processing queue task.
         #may wanna listen to onItemStarted to remove items from the queue that are being updated again to prevent issues.
 
     #add items to queue with the specified date to be processed when the folder is finished syncing
